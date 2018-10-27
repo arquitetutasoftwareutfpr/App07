209c062870bd7f4a970a5b7860636c22ca749753
Vanderley Sousa <vanderley_1998@hotmail.com.br>
2018-10-27 20:10:08 -0300

    Correções em country

--- src/main/java/edu/utfpr/cp/sa/business/CountryBusiness.java
+++ src/main/java/edu/utfpr/cp/sa/business/CountryBusiness.java
@@ -15,20 +15,17 @@ public class CountryBusiness {
     private CountryDAO countryDAO;
 
     @Autowired
-    public CountryBusiness (CountryDAO countryDAO) {
+    public CountryBusiness(CountryDAO countryDAO) {
         this.countryDAO = countryDAO;
     }
-    
-    public boolean create(Country country) throws Exception {
 
-        if (this.read().stream().map(Country::getName).anyMatch(e -> e.equals(country.getName()))) 
+    public boolean create(Country country) throws Exception {
+        if (this.read().stream().map(Country::getName).anyMatch(e -> e.equals(country.getName()))) {
             throw new IllegalArgumentException("There already is a country with this name!");
-        
-        else {
+        } else {
             this.countryDAO.save(country);
             return true;
         }
-            
     }
 
     public List<Country> read() {
@@ -36,29 +33,22 @@ public class CountryBusiness {
     }
 
     public boolean update(Country country) throws Exception {
-        if (this.read().stream().map(Country::getName).anyMatch(e -> e.equals(country.getName()))) 
+        if (this.read().stream().map(Country::getName).anyMatch(e -> e.equals(country.getName()))) {
             throw new IllegalArgumentException("There already is a country with this name!");
-
-        else {
+        } else {
             this.countryDAO.saveAndFlush(country);
             return true;
         }
     }
 
-    public boolean delete(Long id , List<Customer> listCustomer) throws Exception {
-        
+    public boolean delete(Long id, List<Customer> listCustomer) throws Exception {
         Country c = this.read().stream().filter(e -> e.getId() == id).findAny().get();
-        
-        if (listCustomer.stream().filter(e -> e.getCountry().getId() == c.getId()).findAny().get() != null) {
-            
-            throw new IllegalArgumentException("Error! Country is being used.");
-            
-        }else{
-            
-            this.countryDAO.delete(c);    
-        
+        for (Customer cus : listCustomer) {
+            if (cus.getCountry().getId() == c.getId()) {
+                throw new IllegalArgumentException("Error! Country is being used.");
+            }
         }
-        
+        this.countryDAO.delete(c);
         return true;
     }
 
--- src/main/java/edu/utfpr/cp/sa/gui/CountryController.java
+++ src/main/java/edu/utfpr/cp/sa/gui/CountryController.java
@@ -7,29 +7,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import edu.utfpr.cp.sa.business.CountryBusiness;
+import edu.utfpr.cp.sa.business.CustomerBusiness;
 import edu.utfpr.cp.sa.entity.Country;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class CountryController {
 
     private CountryBusiness countryBusiness;
+    private CustomerBusiness customerBusiness;
 
     @Autowired
-    public CountryController (CountryBusiness countryBusiness) {
+    public CountryController(CountryBusiness countryBusiness, CustomerBusiness customerBusiness) {
         this.countryBusiness = countryBusiness;
+        this.customerBusiness = customerBusiness;
 
     }
 
-    @GetMapping ("/country")
+    @GetMapping("/country")
     public String view(Model model) {
-
         model.addAttribute("countryList", countryBusiness.read());
 
         return "country";
     }
 
-    @PostMapping ("/country/new")
-    public String create (CountryDTO country) {
+    @PostMapping("/country/new")
+    public String create(CountryDTO country) {
 
         try {
             Country c = new Country();
@@ -42,14 +47,19 @@ public class CountryController {
         } catch (Exception e) {
             //TODO: handle exception
         }
-        
 
         return "redirect:/country";
     }
-    @DeleteMapping("/{countryId}")
+
+    @GetMapping("/country/delete/{countryId}")
     public String delete(@PathVariable String countryId) {
         long idCountry = Long.parseLong(countryId);
-        countryBusiness.delete(idCountry);
+        try {
+            System.out.println(customerBusiness.read());
+            countryBusiness.delete(idCountry, customerBusiness.read());
+        } catch (Exception ex) {
+            Logger.getLogger(CountryController.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return "redirect:/country";
     }
 }
